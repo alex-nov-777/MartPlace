051368f4c7b3b1956aca78e7817ee20bbf292791
alex-nov-777 <novomlinov.a@yandex.ru>
2020-05-05 21:54:03 +0300

    added main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,44 @@
-$(function)({
+$(function(){
 
-   
+   $('input, select').styler();
+
+   $('.slider-one__inner').slick({
+      dots: false,
+      arrows: true,
+      prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/chevron-left.png" alt=""></button>',
+      nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.png" alt=""></button>',
+   });
+
+   $('.slider-products__inner').slick({
+      dots: false,
+      arrows: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      centerMode: false,
+      prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/chevron-left.png" alt=""></button>',
+      nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.png" alt=""></button>',
+   });
+
+   $(".rate-star").rateYo({
+      // rating: 4.5,
+      starWidth: "15px",
+      readOnly: true,
+      normalFill: "#ffffff",
+      ratedFill: "#ffc000",
+      starSvg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path stroke="#ffc000" d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>',
+   });
+
+   $(".rate-star--products").rateYo({
+      // rating: 4.5,
+      starWidth: "12px",
+      readOnly: true,
+      normalFill: "#ffffff",
+      ratedFill: "#ffc000",
+      starSvg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path stroke="#ffc000" d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>',
+   });
+
+   var mixer = mixitup('.products__inner-box');
 
 });
+
--- gulpfile.js
+++ gulpfile.js
@@ -22,9 +22,13 @@ gulp.task('style', function () {
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/slick-carousel/slick/slick-theme.css',
     'node_modules/magnific-popup/dist/magnific-popup.css',
     'node_modules/rateyo/src/jquery.rateyo.css',
     'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+    'node_modules/rateyo/src/jquery.rateyo.css',
   ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -38,6 +42,8 @@ gulp.task('script', function () {
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
 ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
